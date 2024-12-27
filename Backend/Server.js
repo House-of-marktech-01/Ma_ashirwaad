42c9c7a99be6e0f82f71829a325f4684fe47200c
stuti <talkiewalkie1997@gmail.com>
2024-12-27 16:13:19 +0530

    Created Auth file

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,16 +3,21 @@ import cors from "cors"
 import express from "express"
 import dotenv from "dotenv"
 import useRoutes from "./Routes/user.js"
+import connectDB from "./Config/db.js"
 
 dotenv.config();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({origin: 'http://localhost:5000', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true}));
+
+connectDB();
 
 
 app.get('/',(req, res)=>{
-res.send("Ma Ashirwad")
+res.send("Ma Ashirwad API is working fine")
 })
 
 app.use('/user', useRoutes)
