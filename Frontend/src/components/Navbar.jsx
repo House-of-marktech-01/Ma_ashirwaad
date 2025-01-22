3a28b53d8faf23e260955e905d3ed541b7c462fd
kirtibindalkb770@gmail.com <kirtibindalkb770@gmail.com>
2025-01-22 21:52:48 +0530

    merge

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -150,7 +150,7 @@ export default function Navbar() {
               </form>
 
               <div className="flex justify-start gap-4">
-                {isLoggedIn ? (
+              {auth?.isAuthenticated ? (
                   <>
                     <Link
                       to="/profile"
